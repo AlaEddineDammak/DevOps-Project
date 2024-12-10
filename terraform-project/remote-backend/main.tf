a0ad2f9db24a71871cc97b97c8bdd7673dbd68ec
AlaEddineDammak <alaindammak@gmail.com>
2024-12-10 22:41:17 +0100

    fixing names 2

--- terraform-project/remote-backend/main.tf
+++ terraform-project/remote-backend/main.tf
@@ -1,6 +1,6 @@
 terraform {
  backend "s3" {
-    bucket= "enis-terraform-for-state-file-0125-2a0eebb3"  # Replace with your S3 bucket name
+    bucket= "custom-terraform-state-bucket-123456-9621525d"  # Replace with your S3 bucket name
     key = "aws-backend/terraform.tfstate" # Location of the state file in the bucket
     region = "us-east-1" # AWS region
     dynamodb_table = "terraform-state-locking-1256" # Replace with your DynamoDB table name
