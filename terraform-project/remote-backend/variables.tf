800a5b7c9076715f1e2623a10e7f821cd32619d6
AlaEddineDammak <alaindammak@gmail.com>
2024-12-11 10:04:26 +0100

    fixing ini2t

--- terraform-project/remote-backend/variables.tf
+++ terraform-project/remote-backend/variables.tf
@@ -1,7 +1,7 @@
 variable "s3_bucket_name" {
   description = "Name of the S3 bucket to store Terraform state"
   type        = string
-  default     = "enis-terraform-for-state-file-0125"
+  default     = "enis-terraform-for-state-file-01256"
 }
 variable "dynamodb_table_name" {
   description = "Name of the DynamoDB table for state locking"
